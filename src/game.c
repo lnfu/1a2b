ba1fb8ab2195b22d1844007b81d562b7e7c8fb4d
Enfu Liao <enfu.liao.cs10@nycu.edu.tw>
2022-12-19 23:57:12 +0800

    init game.c game.h

---
+++ src/game.c
@@ -0,0 +1,47 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+
+bool game(char *ans, char *guess, int *a, int *b) {
+  (*a) = (*b) = 0;
+  printf("ANS: %s\n", ans);
+  printf("GUESS: %s\n", guess);
+
+  bool recorded_i[4] = {0};
+  bool recorded_j[4] = {0};
+
+  for (int i = 0; i < 4; i++) {
+    if (ans[i] == guess[i]) {
+      recorded_i[i] = true;
+      recorded_j[i] = true;
+      (*a)++;
+    }
+  }
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      if (recorded_i[i]) continue;
+      if (recorded_j[j]) continue;
+      if (ans[i] == guess[j]) {
+        printf("i = %d\tj = %d\n", i, j);
+        recorded_i[i] = true;
+        recorded_j[j] = true;
+        (*b)++;
+      }
+    }
+  }
+
+
+
+  printf("%dA%dB\n", *a, *b);
+  return *a != 4;
+}
+
+
+void gen_answer(char *ans) {
+  srand(time(NULL));
+  char a[2];
+  for (int i = 0; i < 4; i++) {
+    sprintf(a, "%d", rand() % 10);
+    ans[i] = *a;
+  }
+}
